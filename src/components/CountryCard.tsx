586abea8852ff5c5a606dd1f221004047f46cf9f
zryqv <mehdizibout@gmail.com>
2022-09-08 02:47:56 +0100

    going to sleep

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,5 +1,6 @@
-import Card from "./shared/Card";
-import Image from "next/image";
+import Card from './shared/Card';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 function CountryCard({
   name,
   population,
@@ -13,17 +14,19 @@ function CountryCard({
   capital: string;
   flag: string;
 }) {
+  const router = useRouter();
   return (
     <>
-      <Card className="w-[16.5rem] h-[20.5rem]">
-        <div className="w-full h-1/2  overflow-hidden">
+      <Card className='w-[18rem] h-[20.5rem]'>
+        <div className='w-full h-1/2  overflow-hidden'>
           <Image
-            className="rounded-t-[0.2rem]"
+            onClick={() => router.push(`/${name}`)}
+            className='rounded-t-[0.2rem] cursor-pointer'
             src={flag}
             alt={`The flag of ${name}`}
-            layout="responsive"
-            width="320px"
-            height="213px"
+            layout='responsive'
+            width='320px'
+            height='213px'
             quality={100}
           />
           {/* <img
@@ -32,23 +35,28 @@ function CountryCard({
             className="rounded-t-[0.25rem] "
           /> */}
         </div>
-        <div className="px-4 flex justify-center items-start flex-col h-1/2 w-full">
-          <div className="-mt-3 pb-4 text-xl font-[800]">{name}</div>
-          <div className="">
-            <div className="text-xs tracking-wider font-[300]">
-              <span className="capitalize font-[600] text-sm">
-                population:{" "}
+        <div className='px-4 flex justify-center items-start flex-col h-1/2 w-full'>
+          <div
+            className='-mt-3 pb-4 text-xl font-[800] cursor-pointer'
+            onClick={() => router.push(`/${name}`)}
+          >
+            {name}
+          </div>
+          <div className=''>
+            <div className='text-xs tracking-wider font-[300]'>
+              <span className='capitalize font-[600] text-sm'>
+                population:{' '}
               </span>
               {population.toLocaleString(undefined, {
                 minimumFractionDigits: 2,
               })}
             </div>
-            <div className="text-xs tracking-wider font-[300]">
-              <span className="capitalize font-[600] text-sm">Region: </span>
+            <div className='text-xs tracking-wider font-[300]'>
+              <span className='capitalize font-[600] text-sm'>Region: </span>
               {region}
             </div>
-            <div className="text-xs tracking-wider font-[300]">
-              <span className="capitalize font-[600] text-sm">Capital: </span>
+            <div className='text-xs tracking-wider font-[300]'>
+              <span className='capitalize font-[600] text-sm'>Capital: </span>
               {capital}
             </div>
           </div>
--- src/pages/[country].tsx
+++ src/pages/[country].tsx
@@ -1,4 +1,70 @@
-function Country() {
-  return <div>[country]</div>;
+import { GetServerSideProps } from 'next';
+import Image from 'next/image';
+import { HiArrowNarrowLeft } from 'react-icons/hi';
+import Card from '../components/shared/Card';
+
+function Country({ data }) {
+  const countryInfo = [
+    { property: 'Native Name: ', value: data.nativeName },
+    { property: 'Population: ', value: data.population },
+    { property: 'Region: ', value: data.region },
+    { property: 'Sub Region: ', value: data.subregion },
+    { property: 'Capital: ', value: data.capital },
+    { property: 'Top Level Domain: ', value: data.topLevelDomain[0] },
+    {
+      property: 'Currencies: ',
+      value: data.currencies.map((currency) => currency.name).join(', '),
+    },
+    {
+      property: 'Languages: ',
+      value: data.languages.map((language) => language.name).join(', '),
+    },
+  ];
+  return (
+    <div className='dark:bg-[#202C37] bg-[#FAFAFA] min-h-[calc(100vh-5rem)] max-w-screen'>
+      <div className='py-8 md:px-14 sm:px-9 px-4 lg:px-24'>
+        <Card className='px-6 py-2 flex justify-center items-start w-fit -ml-2 cursor-pointer'>
+          <HiArrowNarrowLeft className='text-2xl mr-4' />
+          Back
+        </Card>
+      </div>
+      <div className='flex justify-between items-start flex-col md:flex-row md:px-14 sm:px-9 px-4 lg:px-24'>
+        <div className='md:w-[40%] md:h-[20rem] '>
+          <Image
+            src={data.flags.png}
+            alt={`The flag of ${data.name}`}
+            layout='responsive'
+            width='320px'
+            height='213px'
+            quality={100}
+          />
+        </div>
+        <div className='flex flex-col justify-between items-start md:w-[50%] md:h-[14rem]  my-auto'>
+          <div className='font-extrabold text-3xl py-6'>{data.name}</div>
+          <ul className='flex flex-col   justify-start items-start flex-wrap w-full md:h-[10rem] '>
+            {/* <li>
+              <span className='font-bold'>Native Name: </span> {data.nativeName}
+            </li> */}
+            {countryInfo.map((info) => (
+              <li key={info.property} className='py-1'>
+                <span className='font-bold'>{info.property}</span> {info.value}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const data = await (
+    await fetch(`https://restcountries.com/v2/name/${context.query.country}`)
+  ).json();
+  // const randomNumber = Math.floor(Math.random() * 241);
+
+  return {
+    props: { data: data[0] }, // will be passed to the page component as props
+  };
+};
+
 export default Country;
